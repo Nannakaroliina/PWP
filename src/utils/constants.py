9a2816f6440c719488817c4665872ed6f9f5f297
Henna Tammia <htammia20@student.oulu.fi>
2022-08-03 17:01:37 +0300

    add constants for resources

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -6,3 +6,11 @@ USER_DELETED = "User deleted."
 INVALID_CREDENTIALS = "Invalid credentials, either username or password isn't correct!"
 USER_LOGGED_OUT = "Successfully logged out."
 LOGIN_SUCCESSFUL = "Login successful."
+
+#Database related constants
+ERROR_INSTERTING = "Could not add to database"
+ERROR_DELETING = "Item could not be deleted"
+ALREADY_EXISTS = "Item by that name already exists"
+
+#JSON related constants
+NOT_JSON = "Request must be JSON"
